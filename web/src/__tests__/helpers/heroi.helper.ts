8b8ae7b983a1061e64c38de5dae418406a184201
cesaraugstz <dev.caugustoaf@gmail.com>
2025-02-21 19:09:54 -0400

    feature: preencher superpoder no teste

--- web/src/__tests__/helpers/heroi.helper.ts
+++ web/src/__tests__/helpers/heroi.helper.ts
@@ -1,8 +1,57 @@
 import { Page } from '@playwright/test';
+import { id } from 'fp-ts/lib/Refinement';
 import { v4 } from 'uuid';
 
 export class HeroiHelper {
   constructor(private page: Page) {}
+
+  obterSuperpoderesBase() {
+    return [
+      {
+        id: 1,
+        nome: 'Super Força',
+        descricao: 'Capacidade de exercer força muito além dos limites humanos',
+      },
+      { id: 2, nome: 'Voo', descricao: 'Habilidade de voar e levitar' },
+      {
+        id: 3,
+        nome: 'Invisibilidade',
+        descricao: 'Poder de ficar invisível aos olhos humanos',
+      },
+    ];
+  }
+
+  private async preencherFormulario(heroi: {
+    nome?: string;
+    nomeHeroi?: string;
+    dataNascimento?: string;
+    altura?: string;
+    peso?: string;
+    superpoderes?: string[]
+  }) {
+    if (heroi.nome) 
+      await this.page.fill('input[formControlName="nome"]', heroi.nome);
+    if (heroi.nomeHeroi)
+      await this.page.fill('input[formControlName="nomeHeroi"]', heroi.nomeHeroi);
+    if (heroi.dataNascimento)
+      await this.page.fill('input[formControlName="dataNascimento"]', heroi.dataNascimento);
+    if (heroi.altura)
+      await this.page.fill('input[formControlName="altura"]', heroi.altura);
+    if (heroi.peso)
+      await this.page.fill('input[formControlName="peso"]', heroi.peso);
+
+    if (heroi.superpoderes?.length) {
+      await this.page.click('mat-select[formControlName="superpoderes"]');
+      for (const poderId of heroi.superpoderes) {
+        await this.page.locator('mat-option', {
+          hasText: poderId,
+        }).click();
+      }
+      await this.page.click('mat-select[formControlName="superpoderes"]', { 
+        force: true 
+      });
+    }
+  }
 
   async criarHeroi(heroi: {
     nome: string;
@@ -10,16 +59,10 @@ export class HeroiHelper {
     dataNascimento: string;
     altura: string;
     peso: string;
+    superpoderes?: string[]
   }) {
     await this.page.click('button:has-text("Adicionar Novo Herói")');
-    await this.page.fill('input[formControlName="nome"]', heroi.nome);
-    await this.page.fill('input[formControlName="nomeHeroi"]', heroi.nomeHeroi);
-    await this.page.fill(
-      'input[formControlName="dataNascimento"]',
-      heroi.dataNascimento
-    );
-    await this.page.fill('input[formControlName="altura"]', heroi.altura);
-    await this.page.fill('input[formControlName="peso"]', heroi.peso);
+    await this.preencherFormulario(heroi);
     await this.page.click('button:has-text("Salvar")');
   }
 
@@ -34,32 +77,13 @@ export class HeroiHelper {
       dataNascimento?: string;
       altura?: string;
       peso?: string;
+      superpoderes?: string[]
     };
   }) {
     await this.page.click(
       `tr:has-text("${nomeHeroiAtual}") button mat-icon:has-text("edit")`
     );
-
-    if (novosDados.nome)
-      await this.page.fill('input[formControlName="nome"]', novosDados.nome);
-    if (novosDados.nomeHeroi)
-      await this.page.fill(
-        'input[formControlName="nomeHeroi"]',
-        novosDados.nomeHeroi
-      );
-    if (novosDados.dataNascimento)
-      await this.page.fill(
-        'input[formControlName="dataNascimento"]',
-        novosDados.dataNascimento
-      );
-    if (novosDados.altura)
-      await this.page.fill(
-        'input[formControlName="altura"]',
-        novosDados.altura
-      );
-    if (novosDados.peso)
-      await this.page.fill('input[formControlName="peso"]', novosDados.peso);
-
+    await this.preencherFormulario(novosDados);
     await this.page.click('button:has-text("Salvar")');
   }
 
@@ -70,6 +94,7 @@ export class HeroiHelper {
       dataNascimento: '01/01/1980',
       altura: '1.88',
       peso: '95',
+      superpoderes: this.obterSuperpoderesBase().map(poder => poder.nome).slice(0, 2)
     };
   }
 
